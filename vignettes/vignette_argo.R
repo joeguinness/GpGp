0ec13ee6e29d68eae89a96258089a2bf6b22d4cc
joeguinness <joeguinness@gmail.com>
2019-05-26 18:05:25 -0400

    argo vignette runs, still need to make .Rmd file

--- vignettes/vignette_argo.R
+++ vignettes/vignette_argo.R
@@ -1,29 +1,8 @@
 
 # analysis of argo data
 devtools::load_all()
-
-read_and_save_data <- FALSE
-if(read_and_save_data){
-argo <- R.matlab::readMat("~/Dropbox/research/fisher_vecchia/data/Argo_data_aggr_02_extended.mat")
-
-inds <- argo$profJulDayAggr > 736200
-
-lon <- argo$profLongAggr[inds]
-lat <- argo$profLatAggr[inds]
-day <- argo$profJulDayAggr[inds]
-n <- length(lon)
-
-temp_prof <- argo$profTempAggr[inds]
-pres_prof <- argo$profPresAggr[inds]
-
-# write code to save the damn data!
-argo2016Jan2Mar <- list( temp_prof = temp_prof, pres_prof = pres_prof, 
-    day = day, lon = lon, lat = lat)
-save(argo2016Jan2Mar, 
-    file = "~/Dropbox/research/fisher_vecchia/data/argo2016Jan2Mar.RData")
-}
-
-load("~/Dropbox/research/fisher_vecchia/data/argo2016Jan2Mar.RData")
+data("argo2016Jan2Mar")
+names(argo2016Jan2Mar)
 attach(argo2016Jan2Mar)
 n <- length(lon)
 
@@ -53,81 +32,42 @@ xyz[,1] <- sin(latrad)*cos(lonrad)
 xyz[,2] <- sin(latrad)*sin(lonrad)
 xyz[,3] <- cos(latrad)
 
-NNarray <- find_ordered_nn(xyz,m=30)
 
-timing <- rep(NA,8)
+
+# subset of data for faster cran checks
+inds <- sample(n,2000)
+
+#NNarray <- find_ordered_nn(xyz,m=30)
+timing <- rep(NA,4)
 
 # spatial isotropic
 t1 <- proc.time()
-fit1 <- fit_model_fisher5(y = temp, X = X, locs = lonlat, 
-    covfun_name = "arma_matern_sphere", group = FALSE, 
-    NNarray = NNarray, reorder = FALSE)
+fit1 <- fit_model(y = temp[inds], X = X[inds,], locs = lonlat[inds,], 
+    covfun_name = "arma_matern_sphere", group = FALSE )
 timing[1] <- (proc.time() - t1)[3]
 
 
 # spacetime, isotropic in each
 t1 <- proc.time()
-fit2 <- fit_model_fisher5(y = temp, X = X, locs = lonlattime, 
-    covfun_name = "arma_matern_spheretime", group = FALSE, 
-    NNarray = NNarray, reorder = FALSE)
+fit2 <- fit_model(y = temp[inds], X = X[inds,], locs = lonlattime[inds,], 
+    covfun_name = "arma_matern_spheretime", group = FALSE )
 timing[2] <- (proc.time() - t1)[3]
 
 
 # spatial warping
 t1 <- proc.time()
-fit3 <- fit_model_fisher5(y = temp, X = X, locs = lonlat, 
-    covfun_name = "arma_matern_sphere_warp", group = FALSE, 
-    NNarray = NNarray, reorder = FALSE)
+fit3 <- fit_model(y = temp[inds], X = X[inds,], locs = lonlat[inds,], 
+    covfun_name = "arma_matern_sphere_warp", group = FALSE )
 timing[3] <- (proc.time() - t1)[3]
 
 
 # spacetime, warping in space
 t1 <- proc.time()
-fit4 <- fit_model_fisher5(y = temp, X = X, locs = lonlattime, 
-    covfun_name = "arma_matern_spheretime_warp", group = FALSE, 
-    NNarray = NNarray, reorder = FALSE)
+fit4 <- fit_model(y = temp[inds], X = X[inds,], locs = lonlattime[inds,], 
+    covfun_name = "arma_matern_spheretime_warp", group = FALSE )
 timing[4] <- (proc.time() - t1)[3]
 
 
 
-
-
-
-# spatial isotropic
-t1 <- proc.time()
-fit5 <- fit_model_arma(y = temp, X = X, locs = lonlat, 
-    covfun_name = "arma_matern_sphere", group = FALSE, 
-    NNarray = NNarray, reorder = FALSE)
-timing[5] <- (proc.time() - t1)[3]
-
-
-# spacetime, isotropic in each
-t1 <- proc.time()
-fit6 <- fit_model_arma(y = temp, X = X, locs = lonlattime, 
-    covfun_name = "arma_matern_spheretime", group = FALSE, 
-    NNarray = NNarray, reorder = FALSE)
-timing[6] <- (proc.time() - t1)[3]
-
-
-# spatial warping
-t1 <- proc.time()
-fit7 <- fit_model_arma(y = temp, X = X, locs = lonlat, 
-    covfun_name = "arma_matern_sphere_warp", group = FALSE, 
-    NNarray = NNarray, reorder = FALSE)
-timing[7] <- (proc.time() - t1)[3]
-
-
-# spacetime, warping in space
-t1 <- proc.time()
-fit8 <- fit_model_arma(y = temp, X = X, locs = lonlattime, 
-    covfun_name = "arma_matern_spheretime_warp", group = FALSE, 
-    NNarray = NNarray, reorder = FALSE)
-timing[8] <- (proc.time() - t1)[3]
-#     user    system   elapsed 
-# 14845.617   645.256 14648.249
-
-
-save(fit1,fit2,fit3,fit4,fit5,fit6,fit7,fit8,timing,
-    file="argo_fitting_results.RData")
 
 
